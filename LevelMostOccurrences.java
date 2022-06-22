46542141c5b9c0e40be0f70cfa701c913659e7ba
kfir7755 <kfir.eliyahu@campus.technion.ac.il>
2022-06-22 20:30:07 +0300

    finished part 1A,added documentation

--- LevelMostOccurrences.java
+++ LevelMostOccurrences.java
@@ -1,6 +1,46 @@
+import java.util.ArrayDeque;
+
 public class LevelMostOccurrences {
     public static int getLevelWithMostOccurrences(BinNode<Integer> node, int num) {
-        // TODO: Add your code here...
-        return 0;
+        ArrayDeque<BinNode> arrayDeque = new ArrayDeque<>();
+        ArrayDeque<BinNode> childrenArrayDeque = new ArrayDeque<>();
+
+        if (node == null) return -1;
+        int size = 1;
+        int levelToReturn = -1;
+        int occurrencesInLevel = 0;
+        int maxOfAllLevels=-1;
+        int currentLevel = 0;
+        arrayDeque.add(node);
+        while (!arrayDeque.isEmpty()) {
+            while (!arrayDeque.isEmpty()) {
+                if (arrayDeque.peek().getLeft() != null)
+                    childrenArrayDeque.add(arrayDeque.peek().getLeft());
+                if (arrayDeque.peek().getRight() != null)
+                    childrenArrayDeque.add(arrayDeque.peek().getRight());
+                arrayDeque.pop();
+            }
+            while (!childrenArrayDeque.isEmpty()) {
+                BinNode<Integer> currentNode = childrenArrayDeque.pop();
+                if (currentNode.getData() == num) {
+                    occurrencesInLevel++;
+                }
+                arrayDeque.add(currentNode);
+            }
+            if (occurrencesInLevel > maxOfAllLevels) {
+                levelToReturn = currentLevel;
+                maxOfAllLevels = occurrencesInLevel;
+            }
+            currentLevel++;
+            occurrencesInLevel=0;
+        }
+
+
+        return levelToReturn;
+//            while (size > 0) {
+//                BinNode binNode = arrayDeque.pop();
+//                size--;
+//            }
+
     }
 }
