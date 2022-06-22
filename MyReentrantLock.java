f5a0fc519caa2c8be87ab08cd7c95f652a4c0b82
kfir7755 <kfir.eliyahu@campus.technion.ac.il>
2022-06-23 02:39:29 +0300

    finished part A, started B need to find problems

--- MyReentrantLock.java
+++ MyReentrantLock.java
@@ -1,22 +1,44 @@
 import java.util.concurrent.atomic.AtomicBoolean;
 
 public class MyReentrantLock implements Lock{
-    private AtomicBoolean isLocked = new AtomicBoolean(false);
+
+    private AtomicBoolean isLocked;
+    private Thread owner;
+
+    public MyReentrantLock() {
+        this.isLocked = new AtomicBoolean(false);
+        this.owner = Thread.currentThread();
+    }
+
     @Override
     public void close(){
         this.isLocked.set(true);
     }
     @Override
     public void acquire(){
-
+        Thread thread = Thread.currentThread();
+             while(thread != owner && this.isLocked.get()) {
+                 //need to find a way to wait without causing a Busy waiting
+             }
+             if (thread == owner) {this.isLocked.compareAndSet(false, true);}
     }
+
     @Override
     public boolean tryAcquire(){
-        return false;
+        boolean check = false;
+        try {
+            check = this.isLocked.compareAndSet(false, true);
+        } finally {
+            return check;
+        }
     }
+
     @Override
     public void release(){
-
+        //the Thread.currentThread() != owner is causing errors for some reason
+        if (this.isLocked.get() == false || Thread.currentThread() != owner) {
+            throw new IllegalReleaseAttempt();
+        }
+        this.isLocked.compareAndSet(true, false);
     }
-
 }
