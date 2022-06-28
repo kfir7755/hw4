34e4f58877c6ce43414f01c27e138bd359df5d84
kfir7755 <kfir.eliyahu@campus.technion.ac.il>
2022-06-28 21:35:18 +0300

    finished part A, started B need to find problems

--- MyReentrantLock.java
+++ MyReentrantLock.java
@@ -2,6 +2,8 @@ import java.util.concurrent.atomic.AtomicBoolean;
 
 public class MyReentrantLock implements Lock{
 
+
+    private int counter=0;
     private AtomicBoolean isLocked;
     private Thread owner = null;
 
@@ -11,38 +13,39 @@ public class MyReentrantLock implements Lock{
 
     @Override
     public void close(){
-        this.isLocked.set(true);
-        this.owner = Thread.currentThread();
+        release();
     }
     @Override
     public void acquire(){
-        Thread thread = Thread.currentThread();
-             if (thread == this.owner && this.owner !=null) {
-                 while (this.isLocked.get()) {
-                    //find a way to make it wait
-                 }
-                 boolean check= this.isLocked.compareAndSet(false, true);
-                 if (check) this.owner = thread;
-             }
+        while (!tryAcquire()){
+            try {
+                Thread.sleep(1);
+            } catch (InterruptedException e){}
+        }
     }
 
     @Override
     public boolean tryAcquire(){
         boolean check = false;
-        try {
-            Thread thread = Thread.currentThread();
-                check = this.isLocked.compareAndSet(false, true);
-                if (check) this.owner = thread;
-            } finally {
-            return check;
+        if (this.isLocked.compareAndSet(false, true) || Thread.currentThread() == this.owner) {
+            this.counter++;
+            this.owner =  Thread.currentThread();
+            check = true;
         }
+        return check;
     }
 
     @Override
     public void release() {
-        if (this.isLocked.get() == false || Thread.currentThread() != owner) {
-            throw new IllegalReleaseAttempt();
+        if (!this.isLocked.get() || Thread.currentThread() != owner || this.counter < 1) {
+           throw new IllegalReleaseAttempt();
         }
-        this.isLocked.compareAndSet(true, false);
+//        if (!this.isLocked.get()) throw new IllegalReleaseAttempt("cant release an unlocked lock");
+//        if (Thread.currentThread() != owner) throw new IllegalReleaseAttempt("cant release if you are not owner");
+//        if (this.counter < 1) throw new IllegalReleaseAttempt("problem with count");
+        if (--this.counter == 0) {
+            this.isLocked = new AtomicBoolean();
+            this.owner = null;
+        }
     }
 }
